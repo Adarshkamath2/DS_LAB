8099ae92c7c685365a3c1f261586b0665de3319d
Adarshkamath2 <89808317+Adarshkamath2@users.noreply.github.com>
2023-02-28 19:57:42 +0530

    Update Time.c

--- Time.c
+++ Time.c
@@ -1,20 +1,25 @@
-// time program
-
+/*
+PROGRAM-1b
+Define a structure called Time containing 3 integer members (Hour, Minute, Second). Develop a menu driven program to perform the following by writing separate function for each operation.
+a) Read (T) :To read time 
+b) Display (T):To display time 
+c) update(T):To Update time 
+d) Add (T1, T2) : Add two time variables.
+Update function increments the time by one second and returns the new time (if the increment results in 60 seconds, then the second member is set to zero and minute member is incremented by one. If the result is 60 minutes, the minute member is set to zero and the hour member is incremented by one. Finally, when the hour becomes 24, Time should be reset to zero. While adding two time variables, normalize the resultant time value as in the case of update function. Note: Illustrate the use of pointer to pass time variable to different functions.
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
 
 struct time
 {
-
-int h,m,s;
+    int H,M,S;
 };
-
 typedef struct time TIME;
 
 void Read(TIME*);
 void Disp(TIME*);
-void update(TIME*);
+void Update(TIME*);
 TIME Add(TIME ,TIME,TIME);
 void init(TIME*);
 
@@ -48,7 +53,7 @@ void main()
 			break;
 
 
-		case 3: update(&t1);
+		case 3: Update(&t1);
 			printf("Updated Time is ....\n");
 			Disp(&t1);
 			break;
@@ -62,53 +67,45 @@ void main()
 	}
 }
 
-void Read(TIME *t)
-	{
-		printf("Enter the Hours , MInute and seconds :\n");
-		scanf("%d%d%d",&t->h,&t->m,&t->s);
-
-	}
-
-void Disp(TIME *t)
+void init(TIME *T)
 {
-
-	printf("%d:%d:%d\n",t->h,t->m,t->s);
-
+    T->S = T->M = T->H = 0;
 }
 
-
-void update(TIME *t)
+void Read(TIME *T)
 {
-	t ->s++;
-	if(t->s>=60)
-	{
-		t->m = t->m+(t->s)/60;
-		t->s = (t->s)%60;
-	}
-	if(t->m >=60)
-	{
-		t->h = t->h+(t->m)/60;
-		t->m = (t->m)%60;
-
-		if(t->h >= 24)
-			init(t);
-	}
+    printf("Enter Time(HH:MM:SS) : ");
+    scanf("%d%d%d",&T->H,&T->M,&T->S);
 }
 
-TIME Add(TIME t1,TIME t2,TIME t3)
+void Disp(TIME *T)
 {
-	t3.h = t1.h + t2.h;
-	t3.m = t1.m + t2.m;
-	t3.s = t1.s + t2.s;
-	t3.s--;
-	update(&t3);
-return t3;
+    printf("Time is - %d:%d:%d",T->H,T->M,T->S);
 }
 
-void init(TIME *t)
+void Update(TIME *T)
 {
-	t->m = t->h = t->s = 0;
+    T->S++;
+    if(T->S >= 60)
+    {
+        T->M = T->M + T->S/60;
+        T->S = T->S%60;
+    }
+    if(T->M >= 60)
+    {
+        T->H = T->H + T->M/60;
+        T->M = T->M%60;
+        if(T->H >=24)
+            init(T);
+    }
 }
 
-
-
+TIME Add(TIME T1,TIME T2,TIME T3)
+{
+    T3.H = T1.H+T2.H;
+    T3.M = T1.M+T2.M;
+    T3.S = T1.S+T2.S;
+    T3.S--;
+    Update(&T3);
+    return T3;
+}
